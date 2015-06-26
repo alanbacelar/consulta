214b89721fdc9c510b8724c66e49b4f9c57e9632
unknown <posgrad@FA7-L30M8.7setlabs.local>
2015-06-25 21:56:01 -0300

    Exemplo Lista e insert

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # consulta
 Trabalho [fa7]
+
+
+### Passo a passo
+
+* Instalar/reinstalar Jboss Tools (ir no marketplace)
+* Ir em Window > Show View > Other e Pesquise por forge
+* Start o servidor e na aba do Forge use:
+
+### Comandos
+
+project-new --named consulta
+
+scaffold-setup 
+
+faces-setup --facesVersion 2.2
---
+++ src/main/java/br/com/fa7/consulta/modelos/Consulta.java
@@ -0,0 +1,42 @@
+package br.com.fa7.consulta.modelos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Consulta {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String nome;
+	private String email;
+	private String senha;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
